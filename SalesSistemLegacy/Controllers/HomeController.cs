e205d7d0db33f28a194467db2d557d7aac072e21
Lucas Cruz <lucas.cruz@bitka.com.br>
2023-07-18 08:08:57 -0300

    First Model-Controller-View - Department

--- SalesSistemLegacy/Controllers/HomeController.cs
+++ SalesSistemLegacy/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesSistemLegacy.Models;
+using SalesSistemLegacy.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
